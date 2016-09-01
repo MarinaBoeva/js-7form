cc00e4c8eaee848e327887620e28359f51d69868
Marina Boeva <camado6pota@yandex.ru>
2016-09-01 12:25:55 +0300

    added new handler

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ $(function () {
   });
   $('.form__element').on('mouseover',showHelp);
   $('.form__element').on('mouseleave',hideHelp);
+
+  $('.form__element').on('focus',showHelp);
+  $('.form__element').on('blur',hideHelp);
+
+  $('.form__element').on('keyup',function(){
+    if (event.which == 9) {
+	        var $next  = $('.form__element').eq($('.form__element').index(this) + 1);
+	        $next.focus();
+	    }
+  });
 });
 function showHelp() {
   $(this).next('.form__help').show(500);
 }
 function hideHelp() {
-  $(this).next('.form__help').hide(500);
+  $(this).next('.form__help').hide(400);
 }
